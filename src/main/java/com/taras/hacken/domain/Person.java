85d9513a44815c3cd0c5e0aeff64ed4696673306
Taras Kurenda <chaserw0w.ti@gmail.com>
2024-07-03 18:00:51 +0300

    added a domain entity

---
+++ src/main/java/com/taras/hacken/domain/Person.java
@@ -0,0 +1,26 @@
+package com.taras.hacken.domain;
+
+import com.taras.hacken.domain.enums.GenderType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private GenderType gender;
+    private String email;
+    private String dateOfBirth;
+    private String countryOfBirth;
+}
---
+++ src/main/java/com/taras/hacken/domain/enums/GenderType.java
@@ -0,0 +1,10 @@
+package com.taras.hacken.domain.enums;
+
+public enum GenderType {
+    MALE("Male"),
+    FEMALE("Female");
+
+    GenderType(String gender) {
+    }
+
+}
